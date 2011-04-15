8cf3227132b9904f845b5cf4b387d9beee67c70e
Joshua Shinavier <josh@fortytwo.net>
2011-04-15 16:04:39 +0800

    Added syntax-level support for line continuation.

--- src/main/java/com/tinkerpop/jig/JigConsole.java
+++ src/main/java/com/tinkerpop/jig/JigConsole.java
@@ -12,12 +12,13 @@ import java.io.InputStream;
 import java.io.OutputStreamWriter;
 
 /**
- * A command-line interpreter/browser which coordinates user interaction with a Ripple query engine.
+ * An interactive command-line interface for <code>JigScriptEngine</code>.
  */
 public class JigConsole {
     private final ScriptEngine scriptEngine;
     private final ConsoleReader reader;
 
+    private String previous = "";
     private int lineNumber = 0;
 
     public JigConsole(final InputStream in,
@@ -25,7 +26,7 @@ public class JigConsole {
         this.scriptEngine = scriptEngine;
         reader = new ConsoleReader(in, new OutputStreamWriter(System.out));
 
-        reader.addCompletor(new SimpleCompletor(new String[] {
+        reader.addCompletor(new SimpleCompletor(new String[]{
                 "Jig",
                 "Graph",
                 ".distinct()",
@@ -41,31 +42,65 @@ public class JigConsole {
     }
 
     public void run() throws ScriptException, IOException, JSONException {
-        while (true) {
-            readLine();
+        while (readLine()) {
         }
     }
 
-    private void readLine() throws ScriptException, IOException, JSONException {
+    private boolean readLine() throws ScriptException, IOException, JSONException {
         ++lineNumber;
-        String prefix = "" + lineNumber + ")  ";
-        String line = reader.readLine(prefix);
-        //System.out.println("done reading the line: " + line);
 
-        if (null != line && line.length() > 0) {
-            System.out.println("");
-            Object result = scriptEngine.eval(line.trim());
-            //System.out.println("[] result: " + result);
-            showResult(result);
-            System.out.println("");
+        String prefix = "jig)  ";
+//        String prefix = "" + lineNumber + ")  ";
+        String line = reader.readLine(prefix);
+
+        return null != line && tryLine(line);
+    }
+
+    private boolean tryLine(final String line) throws ScriptException, JSONException {
+        String s = line.trim();
+
+        boolean cont = s.endsWith("\\");
+        if (cont) {
+            s = s.substring(0, s.length() - 1).trim();
         }
+
+        if (previous.length() > 0) {
+            if (cont) {
+                if (s.length() > 0) {
+                    previous += "\n";
+                }
+                return true;
+            } else {
+                previous = "";
+                return executeScript(previous + s);
+            }
+        } else {
+            if (cont) {
+                if (s.length() > 0) {
+                    previous = s + "\n";
+                }
+                return true;
+            } else {
+                return s.length() == 0 || executeScript(s);
+            }
+        }
+    }
+
+    private boolean executeScript(final String script) throws ScriptException, JSONException {
+        System.out.println("");
+        Object result = scriptEngine.eval(script.trim());
+        showResult(result);
+        System.out.println("");
+
+        // TODO: provide a "quit" command
+        return true;
     }
 
     private void showResult(final Object result) throws JSONException {
         if (result instanceof JSONArray) {
             JSONArray a = (JSONArray) result;
             for (int i = 0; i < a.length(); i++) {
-                System.out.println("  [" + (i+1) + "]  " + a.get(i));
+                System.out.println("  [" + (i + 1) + "]  " + a.get(i));
             }
         } else {
             System.out.println("  [1]  " + result);
--- src/main/javascript/jig-play.js
+++ src/main/javascript/jig-play.js
@@ -2,11 +2,10 @@
 
 g = Jig.Graph();
 
-dailymed = "http://www4.wiwiss.fu-berlin.de/dailymed/resource/dailymed/";
-rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
-
-name = Part.resource(dailymed + "name");
-type = Part.resource(rdf + "type");
+dailymed = "http://www4.wiwiss.fu-berlin.de/dailymed/resource/dailymed/"; \
+rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"; \
+name = Part.resource(dailymed + "name"); \
+type = Part.resource(rdf + "type"); \
 
 r = Part.resource("http://www4.wiwiss.fu-berlin.de/dailymed/resource/drugs/2908");
 
@@ -16,36 +15,33 @@ g.v(r).outE(type).head.eval
 
 
 
-g.triples(r).limit(10).head().eval();
+g.triples(r).limit(10).head.eval
 
 
-g.triples(r, null, null, null).limit(10).tail().distinct().outE.eval();
-
-
-
-
-g.triples(r, null, null, null).limit(10).tail().distinct().outE().eval();
-g.triples(r, null, null, null).limit(10).tail().distinct().inE().eval();
-
-g.triples(r, null, null, null).tail().distinct().outE(null).limit(10).eval();
-g.triples(r, null, null, null).tail().distinct().inE().limit(10).eval();
-
-
-
-g.triples(r, null, null, null).limit(10).tail().distinct().outE(null).path();
-
-
-g.triples(r, null, null, null).limit(10).tail().outE(null).eval();
+g.triples(r).limit(10).tail.distinct.outE.eval
 
 
 
 
-g.triples(r, null, null, null).limit(10).tail().distinct().eval();
-g.triples(r, null, null, null).limit(10).tail().distinct().outE(null).eval();
+g.triples(r).limit(10).tail.distinct.outE.eval
+g.triples(r).limit(10).tail.distinct.inE.eval
+
+g.triples(r).tail.distinct.outE.limit(10).eval
+g.triples(r).tail.distinct.inE.limit(10).eval
 
 
 
-g.single(r).outE(null).eval();
-g.triples(r, null, null, null).eval();
-store.getTriples(r, null, null, null);
+g.triples(r).limit(10).tail.distinct.outE.path
+
+
+g.triples(r).limit(10).tail.outE.eval
+
+
+
+
+g.triples(r).limit(10).tail.distinct.eval
+g.triples(r).limit(10).tail.distinct.outE.eval
+
+
+
 
